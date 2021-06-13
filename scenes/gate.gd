cea689842871b6f53c6c0e39ec94ce7206ea21e9
Zaven Muradyan <zaven@voithos.io>
2021-06-13 02:14:08 -0700

    Add nodust versions of gate anims.

--- scenes/gate.gd
+++ scenes/gate.gd
@@ -2,6 +2,7 @@ extends Sprite
 
 export var is_open = false
 export var is_latchable = false # "Latches" in a stuck position once it is engaged / disengaged
+export var nodust = false
 var is_latched = false
 
 onready var default_state = is_open
@@ -12,9 +13,9 @@ var last_anim = ""
 func _ready():
     add_to_group("gates")
     if is_open:
-        $animation.play("close")
+        $animation.play(_anim_name("close"))
     else:
-        $animation.play("open")
+        $animation.play(_anim_name("open"))
     
     $animation.seek(0, true)
     $animation.stop()
@@ -26,16 +27,21 @@ func _input(event):
 #        toggle()
     pass
 
+func _anim_name(anim):
+    if nodust:
+        return anim + "_nodust"
+    return anim
+
 func open():
     if !is_open and !is_latched:
-        _queue_anim("open")
+        _queue_anim(_anim_name("open"))
         is_open = true
         if is_latchable:
             is_latched = true
 
 func close():
     if is_open and !is_latched:
-        _queue_anim("close")
+        _queue_anim(_anim_name("close"))
         is_open = false
         if is_latchable:
             is_latched = true
